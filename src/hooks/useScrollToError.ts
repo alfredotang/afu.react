65d51db3bd8f40056732fd24bc2d86b5ff09412b
alfredotang <asdzxcca0617@gmail.com>
2021-04-14 15:35:08 +0800

    feat(useScrollToError): 優化 scroll 到的位置

--- src/hooks/useScrollToError.ts
+++ src/hooks/useScrollToError.ts
@@ -5,13 +5,19 @@ type UseScrollToErrorProps<T> = {
   errors: DeepMap<T, FieldError>;
 };
 
-// FIXME: not work XD
+/**
+ * @description auto scroll to error
+ * @param errors react-hook-form -> const { formState: { errors }} = (useForm | useFormContext)
+ */
 function useScrollToError<T>({ errors }: UseScrollToErrorProps<T>): void {
   useEffect(() => {
     // console.log({ ALL_ERROR: errors });
     const keys = Object.keys(errors);
     if (keys.length > 0) {
-      errors[keys[0]].ref.scrollIntoView({ behavior: 'smooth' });
+      errors[keys[0]].ref.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
     }
   }, [errors]);
 }
