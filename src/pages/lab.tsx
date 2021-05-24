cf9e0bdca45fe8c78f78822495855132ccc1004c
alfredotang <asdzxcca0617@gmail.com>
2021-05-24 17:03:34 +0800

    feat: add date time picker lab

--- src/pages/lab.tsx
+++ src/pages/lab.tsx
@@ -1,7 +1,10 @@
 import type { FC, ChangeEvent } from 'react';
 import { useState } from 'react';
-import { Select } from '@src/components';
+import { dayjs } from '@src/providers';
+import { Select, DateTimePicker } from '@src/components';
 import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Divider from '@material-ui/core/Divider';
 
 const source: IKeyValuePair<string, string>[] = [
   { key: 'a', value: 'A' },
@@ -10,9 +13,23 @@ const source: IKeyValuePair<string, string>[] = [
 
 const Lab: FC = () => {
   const [selectValue, setSelectValue] = useState<string>('');
+  const [dateTimeDefault, setDateTimeDefault] = useState<Date>(null);
+  const [date, setDate] = useState<Date>(null);
+  const [time, setTime] = useState<Date>(null);
 
   const handleChangeSelected = (event: ChangeEvent<{ value: unknown }>) => {
     setSelectValue(event.target.value as string);
+  };
+
+  const handleChangeDateTimeDefault = (date: Date) => {
+    console.log({ date });
+    setDateTimeDefault(date);
+  };
+  const handleChangeDate = (date: Date) => {
+    setDate(date);
+  };
+  const handleChangeTime = (date: Date) => {
+    setTime(date);
   };
 
   return (
@@ -23,6 +40,51 @@ const Lab: FC = () => {
         value={selectValue}
         onChange={handleChangeSelected}
       />
+      <DateTimePicker
+        value={dateTimeDefault}
+        placeholder="請選擇"
+        onChange={handleChangeDateTimeDefault}
+        min={new Date()}
+      />
+      <pre>
+        <code>{JSON.stringify(dateTimeDefault)}</code>
+        <br />
+        <code>{JSON.stringify(typeof dateTimeDefault)}</code>
+      </pre>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <DateTimePicker
+          variant="date"
+          placeholder="請選擇"
+          value={date}
+          onChange={handleChangeDate}
+          min={new Date()}
+        />
+        <Divider orientation="vertical" />
+        <DateTimePicker
+          variant="date"
+          placeholder="請選擇"
+          value={date}
+          onChange={handleChangeDate}
+          min={new Date()}
+        />
+      </div>
+      <pre>
+        <code>{JSON.stringify(date)}</code>
+        <br />
+        <code>{JSON.stringify(typeof date)}</code>
+      </pre>
+      <DateTimePicker
+        placeholder="請選擇"
+        variant="time"
+        value={time}
+        onChange={handleChangeTime}
+        min={new Date()}
+      />
+      <pre>
+        <code>{JSON.stringify(time)}</code>
+        <br />
+        <code>{JSON.stringify(typeof time)}</code>
+      </pre>
     </Box>
   );
 };
