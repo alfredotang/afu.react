152e0d6e9d02c883d9cf820f87343fa7e612452e
alfredotang <asdzxcca0617@gmail.com>
2021-05-31 15:49:12 +0800

    feat: add checkbox

---
+++ src/components/checkbox/index.tsx
@@ -0,0 +1,58 @@
+import type { ForwardRefExoticComponent, ReactNode, ChangeEvent } from 'react';
+import type { SxProps } from '@material-ui/system';
+import type { Theme } from '@material-ui/core/styles';
+import type {
+  CheckboxProps as MuiCheckboxProps,
+  FormControlLabelProps,
+} from '@material-ui/core';
+import { forwardRef } from 'react';
+
+import MuiCheckbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+
+export type CheckboxProps = Omit<MuiCheckboxProps, 'onChange' | 'checked'> & {
+  sx?: SxProps<Theme>;
+  error?: boolean;
+  helperText?: string;
+  label: ReactNode;
+  onChange?: (checked: boolean) => void;
+  value: boolean;
+};
+
+export const Checkbox: ForwardRefExoticComponent<CheckboxProps> = forwardRef(
+  (props, ref) => {
+    const { sx, error, helperText, value, name, label, onChange } = props;
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      onChange(event.target.checked);
+    };
+
+    return (
+      <FormControl sx={sx}>
+        <FormControlLabel
+          control={
+            <MuiCheckbox
+              sx={{ color: (theme) => error && theme.palette.error.main }}
+              checked={value}
+              onChange={handleChange}
+              name={name}
+              ref={ref}
+            />
+          }
+          label={label}
+        />
+        {error && helperText && (
+          <FormHelperText
+            sx={{ marginLeft: 0, color: (theme) => theme.palette.error.main }}
+          >
+            {helperText}
+          </FormHelperText>
+        )}
+      </FormControl>
+    );
+  }
+);
+
+export default Checkbox;
---
+++ src/components/checkboxForForm/index.tsx
@@ -0,0 +1,31 @@
+import type { FC } from 'react';
+import { useController, useFormContext } from 'react-hook-form';
+
+import { Checkbox, CheckboxProps } from '@src/components/checkbox';
+
+type CheckboxForFormForFormProps = Omit<
+  CheckboxProps,
+  'error' | 'helperText' | 'value' | 'onChange' | 'ref' | 'name'
+> & {
+  name: string;
+};
+
+export const CheckboxForForm: FC<CheckboxForFormForFormProps> = (props) => {
+  const { control } = useFormContext();
+  const { name } = props;
+  const {
+    field,
+    fieldState: { error },
+  } = useController({ name, control });
+
+  return (
+    <Checkbox
+      {...props}
+      {...field}
+      error={Boolean(error)}
+      helperText={error?.message || ''}
+    />
+  );
+};
+
+export default CheckboxForForm;
