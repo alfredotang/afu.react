143a1ef60196c9332ade6c529b45d590ef45d7fe
alfredotang <asdzxcca0617@gmail.com>
2021-06-17 18:08:24 +0800

    fix: deploy path error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
+const prod = process.env.NODE_ENV === 'production'
 
-const dayjs = require('dayjs');
-
-const env =  process.env.NODE_ENV || 'dev';
+console.log({env: process.env.NODE_ENV})
 
 module.exports = {
   distDir: 'build',
-  assetPrefix: env === 'dev' ? '' : '/afu.react',
+  assetPrefix: prod ?  '/afu.react' : '',
   reactStrictMode: true,
-  generateBuildId: async () => {
-    return `0.1.0-${dayjs().format('YYYY/MM/DD-HH:mm')}`;
-  },
-  basePath: env === 'dev' ? '' : '/afu.react',
+  basePath: prod ?  '/afu.react' : '',
 };
