86abddd33705b4f66ecf987e2263ee98bed23e14
alfredotang <asdzxcca0617@gmail.com>
2021-06-17 17:13:14 +0800

    feat: rm lab testing code

--- src/pages/lab.tsx
+++ src/pages/lab.tsx
@@ -1,25 +1,11 @@
 import type { FC, ChangeEvent } from 'react';
 import { useState } from 'react';
-import { dayjs } from '@src/providers';
 import {
-  Select,
-  DateTimePicker,
-  DateTimeRangePicker,
   ChipInput,
 } from '@src/components';
 import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
-import Divider from '@material-ui/core/Divider';
-
-const source: IKeyValuePair<string, string>[] = [
-  { key: '321134', value: 'A' },
-  { key: 'b24343214', value: 'B' },
-  { key: 'csadsac', value: 'C' },
-  { key: 'das1232', value: 'D' },
-];
 
 const Lab: FC = () => {
-  const [selectValue, setSelectValue] = useState<string[]>([]);
   const [value, setValue] = useState<string[]>([]);
 
   const handleAdd = (chip: string[]) => {
@@ -43,18 +29,6 @@ const Lab: FC = () => {
       </Box>
       <pre>
         <code>{JSON.stringify(value)}</code>
-      </pre>
-      <Select
-        source={source}
-        placeholder="請選擇"
-        value={selectValue}
-        onChange={(event) => setSelectValue(event.target.value as string[])}
-        multiple
-      />
-      <pre>
-        <code>{JSON.stringify(selectValue)}</code>
-        <br />
-        <code>{JSON.stringify(typeof selectValue)}</code>
       </pre>
     </>
   );
