3a69b58d99c7c22179884ad103a2adf35e11316f
alfredotang <asdzxcca0617@gmail.com>
2021-06-17 18:28:10 +0800

    chore: next.config show env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const prod = process.env.NODE_ENV === 'production'
 
-console.log({env: process.env.NODE_ENV})
+console.log('-------------------------');
+console.log(`\x1b[32m env:\x1b[0m ${process.env.NODE_ENV}`);
+console.log(`\x1b[32m isProd:\x1b[0m ${prod}`);
+console.log(`\x1b[32m isDev:\x1b[0m ${!prod}`);
+console.log('-------------------------');
+
 
 module.exports = {
   distDir: 'build',
