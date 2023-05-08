75054c93a446c1861a1fe8bbc2a1143f1f4a1a3c
alfredotang <asdzxcca0617@gmail.com>
2023-05-08 15:45:51 +0000

    Deploying to gh-pages from @ alfredotang/afu.react@f22683cf523c01b524fe16bae6f3c416d0cd3207 🚀

---
+++ _next/static/chunks/pages/index-d3de19b4e4098012.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{886:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(1721)}])},1721:function(n,u,_){"use strict";_.r(u);var e=_(1527);let t=()=>(0,e.jsx)("div",{children:"Home"});u.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=886)}),_N_E=n.O()}]);
